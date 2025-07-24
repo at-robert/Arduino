bf9c69d848500b5ee3775e7b4962b1a1709eda74
robert.lo <robert.lo@amtran.com.tw>
2025-07-24 13:37:47 +0800

    [import] - To add Mini LED SPI and GPIO control

---
+++ MiniLED_BCON_Control/MiniLED_BCON_Control.ino
@@ -0,0 +1,58 @@
+#include <SPI.h>
+
+#define PIN_VSYNC   D2      // GPIO4
+#define PIN_CS    D8      // GPIO15
+
+void setup() {
+  Serial.begin(115200);
+
+   // 初始化 SPI
+  SPI.begin(); // 預設 SCLK:14, MISO:12, MOSI:13
+
+  // 設定 GPIO 為輸出
+  pinMode(PIN_VSYNC, OUTPUT);
+  pinMode(PIN_CS, OUTPUT);
+
+  digitalWrite(PIN_VSYNC, LOW);
+  digitalWrite(PIN_CS, HIGH);  // CS 初始為高（閒置）
+
+  digitalWrite(PIN_VSYNC, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(PIN_VSYNC, LOW);
+  delayMicroseconds(100);
+  //delay(1);
+
+  //delayMicroseconds(100);
+}
+
+void spiSendWord(uint16_t data) {
+  SPI.transfer(highByte(data));  // 傳送高位元組
+  SPI.transfer(lowByte(data));   // 傳送低位元組
+}
+
+
+void loop() {
+  int i = 0;
+
+  // 使用 SPI 傳送資料
+  SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));    // 8 MHz
+  digitalWrite(PIN_CS, LOW);           // 啟動 SPI 裝置
+  spiSendWord(0xAA55);
+  spiSendWord(0x0000);
+
+  for(i =0 ;i < 1152 ; i++){
+    spiSendWord(0xFF00); //1152個
+  }
+  
+  spiSendWord(0xAA55);
+  
+  
+  // 傳送一個 byte
+  digitalWrite(PIN_CS, HIGH);          // 關閉 SPI 裝置
+  SPI.endTransaction(); //傳輸結束
+
+  digitalWrite(PIN_VSYNC, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(PIN_VSYNC, LOW);
+  delayMicroseconds(100);
+}
