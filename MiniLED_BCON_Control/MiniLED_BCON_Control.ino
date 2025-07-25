6592b54e54d1f1df2468ebadc822f762b63fd887
robert.lo <robert.lo@amtran.com.tw>
2025-07-25 15:05:58 +0800

    [refactor] - To modify the code

--- MiniLED_BCON_Control/MiniLED_BCON_Control.ino
+++ MiniLED_BCON_Control/MiniLED_BCON_Control.ino
@@ -17,7 +17,7 @@ void setup() {
   digitalWrite(PIN_CS, HIGH);  // CS 初始為高（閒置）
 
   digitalWrite(PIN_VSYNC, HIGH);
-  delayMicroseconds(5);
+  delayMicroseconds(20);
   digitalWrite(PIN_VSYNC, LOW);
   delayMicroseconds(100);
   //delay(1);
@@ -33,18 +33,30 @@ void spiSendWord(uint16_t data) {
 
 void loop() {
   int i = 0;
+  uint16_t chk_sum = 0x0000,indor = 0x00000, cmd = 0x0000,dat_buf = 0x0000;
 
   // 使用 SPI 傳送資料
-  SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));    // 8 MHz
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));    // 8 MHz
   digitalWrite(PIN_CS, LOW);           // 啟動 SPI 裝置
-  spiSendWord(0xAA55);
-  spiSendWord(0x0000);
+  delayMicroseconds(10);
 
+  indor = 0xAA55; // indicator
+  cmd = 0x0000; // command
+  dat_buf = 0xFF00; // Data
+  chk_sum = 0x0000; // Checksum
+  
+  spiSendWord(indor);
+  chk_sum = chk_sum ^ indor;
+  
+  spiSendWord(cmd);
+  chk_sum = chk_sum ^ cmd;
+  
   for(i =0 ;i < 1152 ; i++){
-    spiSendWord(0xFF00); //1152個
+    spiSendWord(dat_buf); //1152個
+    chk_sum = chk_sum ^ dat_buf;
   }
   
-  spiSendWord(0xAA55);
+  spiSendWord(chk_sum);
   
   
   // 傳送一個 byte
@@ -52,7 +64,7 @@ void loop() {
   SPI.endTransaction(); //傳輸結束
 
   digitalWrite(PIN_VSYNC, HIGH);
-  delayMicroseconds(5);
+  delayMicroseconds(20);
   digitalWrite(PIN_VSYNC, LOW);
   delayMicroseconds(100);
 }
